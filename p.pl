6ca2ab3f73e4445237b6a37d686acc636b5fda4c
RevMatres <benediktheinen1@gmail.com>
2018-09-02 17:28:59 +0200

    added everything

---
+++ p.pl
@@ -0,0 +1,72 @@
+#!/bin/perl
+
+use strict;
+use warnings;
+use 5.010;
+
+# get sections and items to match against
+open(my $ini_fd, "<", "sections.ini") or die "couldn't read ini";
+
+# get data
+open(my $data_fd, "<", "test.csv")    or die "couldn't read data";
+my @data = <$data_fd>;
+
+# init data structures
+my %data_struct = ();
+my $current_section = "Data";
+
+foreach (<$ini_fd>) {
+
+  # match
+  given($_) {
+  
+    # if section header
+    when(/\[(.*)\]/)  {
+
+      # add to hash
+      $data_struct{$1} = [];
+
+      # set current section
+      $current_section = $1;
+    }
+    
+    # if item that is to be matched
+    when(/(.+)/)      {
+      my $item = $1;
+
+      # match data against item
+      foreach (@data) {
+        given($_) {
+
+          # if item is found in the data
+          when(/(.*$item.*)/) {
+            # append to file structure
+            push @{ $data_struct{$current_section} }, $1;
+          }
+
+          # if item isn't found, do nothing
+          default {}
+        }
+      }
+
+    }
+    default {}
+  }
+}
+
+foreach (keys %data_struct) {
+
+  # create file
+  open(my $output_fd, ">>", "$_.csv")   or die "couldn't open output file $_.csv";
+
+  # print output to file
+  foreach (@{ $data_struct{$_} }) {
+          print $output_fd "$_\n";
+  }
+
+  # close fd
+  close $output_fd  or die "couldn't close output fd";
+}
+
+close $ini_fd       or die "couldn't close fd";
+close $data_fd      or die "couldn't close fd";
